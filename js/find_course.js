c3c4063a2609cd8e5e9377d1feac698a69aff8bc
Michelle Duer <michelle.duer@gmail.com>
2017-08-28 14:47:46 -0700

    created and connected to mysql database

---
+++ js/find_course.js
@@ -0,0 +1,11 @@
+
+alert("DO THIS FOR OLIVIA!");
+
+$(function() {
+    functionNoMatch();
+    //var item = document.getElementById("courseSystem").selectedIndex;
+});
+
+var functionNoMatch = function() {
+    alert("accessing the correct function!");
+};
--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 'use strict';
 
 var express = require('express');
+var mysql = require('mysql')
 var server = express();
 var path = require('path');
 
-server.get('/course_entry.html', function(req, res) {
+/*
+* SERVER CONNECTION
+ */
+server.get('/find_course.html', function(req, res) { // help from Simon
     res.set({
         'Content-Type': 'text/html'
     });
-    res.sendFile(path.join(__dirname + '/course_entry.html'));
+    res.sendFile(path.join(__dirname + '/html/find_course.html'));
 });
 
-server.use('/', express.static(__dirname + '/'));
+server.use('/', express.static(__dirname + '/')); // help from Simon
+
+
+/*
+* DATABASE CONNECTION
+*/
+var db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'student',
+    password : 'secret',
+    database : 'datag'
+});
+
+db.connect()
+
+db.query('SELECT * FROM courses', function (err, rows, fields) {
+    if (err) throw err
+
+    console.log('The courses table contains: ', rows)
+})
+
+db.end()
 
 server.listen(8080);
